6da78e63a05bf974a2065540e577b02f70aeb58e
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-09-11 12:07:12 +0200

    + metadata serializable + misa_json_file

--- misaxx/metadata/object3d_pixels.h
+++ misaxx/metadata/object3d_pixels.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include <nlohmann/json.hpp>
+
 namespace misaxx::metadata {
     /**
      * Attachable metadata containing the number of pixels of a 3D object
@@ -12,4 +14,12 @@ namespace misaxx::metadata {
     struct object3d_pixels {
         int pixels = 0;
     };
+
+    void to_json(nlohmann::json &j, const object3d_pixels &o) {
+        j["pixels"] = o.pixels;
+    }
+
+    void from_json(const nlohmann::json &j, object3d_pixels &o) {
+        o.pixels = j["pixels"];
+    }
 }
---
+++ misaxx/module_data/misa_json_file.h
@@ -0,0 +1,31 @@
+//
+// Created by rgerst on 11.09.18.
+//
+
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include "misa_file.h"
+#include <fstream>
+
+namespace misaxx {
+    /**
+     * misa_file with additional operators for easier serialization of JSON data
+     */
+    struct misa_json_file : public misa_file {
+        using misa_file::misa_file;
+
+        void operator << (const nlohmann::json &json) {
+            std::ofstream sw;
+            sw.open(path.string());
+            sw << json;
+        }
+
+        void operator >> (nlohmann::json &json) {
+            std::ifstream sr;
+            sr.open(path.string());
+            sr >> json;
+        }
+    };
+}
