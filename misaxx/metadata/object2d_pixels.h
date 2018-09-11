004e421c6a7de3765e82b9eb0b33c7c7f06ca866
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-09-11 10:33:56 +0200

    + metadata

---
+++ misaxx/metadata/object2d_pixels.h
@@ -0,0 +1,15 @@
+//
+// Created by rgerst on 11.09.18.
+//
+
+
+#pragma once
+
+namespace misaxx::metadata {
+    /**
+     * Attachable metadata containing the number of pixels of a 2D object
+     */
+    struct object2d_pixels {
+        int pixels = 0;
+    };
+}
---
+++ misaxx/metadata/object3d_pixels.h
@@ -0,0 +1,15 @@
+//
+// Created by rgerst on 11.09.18.
+//
+
+
+#pragma once
+
+namespace misaxx::metadata {
+    /**
+     * Attachable metadata containing the number of pixels of a 3D object
+     */
+    struct object3d_pixels {
+        int pixels = 0;
+    };
+}
