e12802159a9d356324c436b48995af7cf43b1847
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-09-17 16:06:54 +0200

    * fix

---
+++ misaxx/metadata/object3d_pixel_bounds.h
@@ -0,0 +1,36 @@
+//
+// Created by rgerst on 17.09.18.
+//
+
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include "misa_metadata.h"
+
+namespace misaxx::metadata {
+    class object3d_pixel_bounds : public misa_metadata {
+
+        int min_x = 0;
+        int min_z = 0;
+        int min_y = 0;
+        int max_x = 0;
+        int max_z = 0;
+        int max_y = 0;
+
+        nlohmann::json to_json() const override {
+            nlohmann::json j;
+            j["min-x"] = min_x;
+            j["min-y"] = min_y;
+            j["min-z"] = min_z;
+            j["max-x"] = max_x;
+            j["max-y"] = max_y;
+            j["max-z"] = max_z;
+            return j;
+        }
+
+        std::string get_name() const override {
+            return "object3d-pixel-bounds";
+        }
+    };
+}
