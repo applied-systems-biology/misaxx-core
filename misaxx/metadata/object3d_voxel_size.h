2586adb7e78d2aead196b845bb277d3dc56c5c45
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-10-01 16:37:12 +0200

    * fix

--- misaxx/metadata/object3d_voxel_size.h
+++ misaxx/metadata/object3d_voxel_size.h
@@ -47,8 +47,21 @@ namespace misaxx {
     }
 
     void from_json(const nlohmann::json& j, object3d_voxel_size& p) {
-        p.x = j["x"];
-        p.y = j["y"];
-        p.z = j["z"];
+        nlohmann::json::const_iterator xz = j.find("xz");
+        nlohmann::json::const_iterator x = j.find("x");
+        nlohmann::json::const_iterator y = j.find("y");
+        nlohmann::json::const_iterator z = j.find("z");
+        if(xz != j.end()) {
+            p.x = p.z = xz.value();
+        }
+        if(x != j.end()) {
+            p.x = x.value();
+        }
+        if(y != j.end()) {
+            p.y = y.value();
+        }
+        if(z != j.end()) {
+            p.z = z.value();
+        }
     }
 }
