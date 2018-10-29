2b1038dc731830e9a1bf86425cc60086ff9fdd2f
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-10-29 15:50:46 +0100

    * fix

--- misaxx/filesystem/misa_filesystem_metadata.h
+++ misaxx/filesystem/misa_filesystem_metadata.h
@@ -23,6 +23,15 @@ namespace misaxx {
          */
         std::string data_type;
 
+        misa_filesystem_metadata() = default;
+
+        template<typename Arg, typename... Args> explicit misa_filesystem_metadata(Arg arg, Args... args) {
+            describe(arg);
+            if constexpr (sizeof...(Args) > 0) {
+                misa_filesystem_metadata(std::forward<Args>(args)...);
+            }
+        }
+
         /**
          * If true, this metadata is empty
          * @return
@@ -63,8 +72,8 @@ namespace misaxx {
          * @tparam Metadata
          * @return
          */
-        template <class Metadata> Metadata &describe(const Metadata &t_description) {
-            m_metadata_instances.access<Metadata>() = t_description;
+        template <class Metadata> Metadata &describe(Metadata &&t_description) {
+            m_metadata_instances.access<Metadata>() = std::forward<Metadata>(t_description);
             return m_metadata_instances.access<Metadata>();
         }
 
