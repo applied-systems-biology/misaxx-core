8a8100925f0b4937637ee7d72f86bc14700bdb3d
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-11-09 16:24:29 +0100

    * fix

--- misaxx/misa_future_dispatch.h
+++ misaxx/misa_future_dispatch.h
@@ -26,11 +26,11 @@ namespace misaxx {
 
         misa_future_dispatch() = default;
 
-//        template<class HigherOrderInstace>
-//        misa_future_dispatch(const misa_future_dispatch<Worker, HigherOrderInstace> &t_src)
-//                : name(t_src.name), function(t_src.function) {
-//
-//        }
+        template<class HigherOrderInstace>
+        misa_future_dispatch(const misa_future_dispatch<Worker, HigherOrderInstace> &t_src)
+                : name(t_src.name), function(t_src.function) {
+
+        }
 
         Instance &dispatch_specific(Worker &t_parent) const {
             return function(t_parent);
