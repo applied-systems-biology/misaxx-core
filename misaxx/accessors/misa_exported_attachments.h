4d576871be7b0d99f1dc12fca61829b35be3556a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-11-28 16:21:03 +0100

    + convenience types

--- misaxx/accessors/misa_exported_attachments.h
+++ misaxx/accessors/misa_exported_attachments.h
@@ -7,12 +7,14 @@
 #include <misaxx/caches/misa_exported_attachments_cache.h>
 #include <misaxx/misa_cached_data.h>
 #include <opencv2/opencv.hpp>
+#include <misaxx/misa_default_description_accessors.h>
 
 namespace misaxx {
     /**
      * Allows exporting attachments to a specified file
      */
-    struct misa_exported_attachments : public misa_cached_data<misa_exported_attachments_cache> {
+    struct misa_exported_attachments : public misa_cached_data<misa_exported_attachments_cache>,
+            public misa_description_accessors_from_cache<misa_exported_attachments_cache, misa_exported_attachments> {
     };
 }
 
--- misaxx/accessors/misa_image_file.h
+++ misaxx/accessors/misa_image_file.h
@@ -6,9 +6,16 @@
 
 #include <misaxx/misa_cached_data.h>
 #include <misaxx/caches/misa_image_file_cache.h>
+#include <misaxx/misa_default_description_accessors.h>
 
 namespace misaxx {
-    template<class Image> struct misa_image_file : public misa_cached_data<misa_image_file_cache<Image>>{
+
+    /**
+     * A simple 2D image file
+     * @tparam Image cv::Mat or any of coixx::image<C>
+     */
+    template<class Image> struct misa_image_file : public misa_cached_data<misa_image_file_cache<Image>>,
+                                                   public misa_description_accessors_from_cache<misa_image_file_cache<Image>, misa_image_file<Image>> {
         /**
          * Clones the image content read from access_readonly()
          * @return
