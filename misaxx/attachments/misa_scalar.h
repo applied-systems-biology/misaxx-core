d9a4c7cfc2ce9bbd8bedfe2fbb8f291d162523dd
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-12-05 16:08:40 +0100

    + misa_scalar

--- misaxx/attachments/misa_scalar.h
+++ misaxx/attachments/misa_scalar.h
@@ -6,38 +6,39 @@
 
 
 #include <misaxx/misa_primitive.h>
+#include <boost/operators.hpp>
 
 namespace misaxx {
+
+    /**
+     * Helper that adds boost operators to the target
+     * @tparam Target
+     * @tparam T
+     */
+    template<class Target, typename T> struct scalar_operators : public boost::partially_ordered<Target,
+                                                     boost::equality_comparable<Target,
+                                                     boost::addable<Target,
+                                                     boost::subtractable<Target,
+                                                     boost::dividable<Target, T,
+                                                     boost::multipliable2<Target, T,
+                                                     boost::incrementable<Target,
+                                                     boost::decrementable<Target>>>>>>>> {
+        
+    };
 
     /**
      * A scalar value that supports calculation and relational operators, but can also be serialized
      * @tparam T
      */
-    template<typename T> struct misa_scalar : public misa_primitive<T> {
+    template<typename T> struct misa_scalar : public misa_primitive<T>, public scalar_operators<misa_scalar<T>, T> {
         using misa_primitive<T>::misa_primitive;
 
         bool operator==(const misa_scalar &rhs) const {
             return this->value == rhs.value;
         }
 
-        bool operator!=(const misa_scalar &rhs) const {
-            return !(rhs == *this);
-        }
-
         bool operator<(const misa_scalar &rhs) const {
             return this->value < rhs.value;
-        }
-
-        bool operator>(const misa_scalar &rhs) const {
-            return rhs < *this;
-        }
-
-        bool operator<=(const misa_scalar &rhs) const {
-            return !(rhs < *this);
-        }
-
-        bool operator>=(const misa_scalar &rhs) const {
-            return !(*this < rhs);
         }
 
         misa_scalar<T>& operator+=(const misa_scalar<T>& rhs) {
@@ -45,23 +46,9 @@ namespace misaxx {
             return *this;
         }
 
-        friend misa_scalar<T> operator+(misa_scalar<T> lhs,
-                           const misa_scalar<T>& rhs)
-        {
-            lhs += rhs;
-            return lhs;
-        }
-
         misa_scalar<T>& operator-=(const misa_scalar<T>& rhs) {
             this->value -= rhs.value;
             return *this;
-        }
-
-        friend misa_scalar<T> operator-(misa_scalar<T> lhs,
-                                        const misa_scalar<T>& rhs)
-        {
-            lhs -= rhs;
-            return lhs;
         }
 
         misa_scalar<T>& operator*=(const misa_scalar<T>& rhs) {
@@ -69,36 +56,9 @@ namespace misaxx {
             return *this;
         }
 
-        friend misa_scalar<T> operator*(misa_scalar<T> lhs,
-                                        const misa_scalar<T>& rhs)
-        {
-            lhs *= rhs;
-            return lhs;
-        }
-
         misa_scalar<T>& operator/=(const misa_scalar<T>& rhs) {
             this->value /= rhs.value;
             return *this;
-        }
-
-        friend misa_scalar<T> operator/(misa_scalar<T> lhs,
-                                        const misa_scalar<T>& rhs)
-        {
-            lhs /= rhs;
-            return lhs;
-        }
-
-        misa_scalar<T>& operator++()
-        {
-            // actual increment takes place here
-            return *this;
-        }
-
-        misa_scalar<T> operator++(int)
-        {
-            misa_scalar<T> tmp(*this); // copy
-            operator++(); // pre-increment
-            return tmp;   // return old value
         }
     };
 
