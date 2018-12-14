32a0430b7744f7f7f23c640f6607c807383bee1b
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-12-14 17:30:23 +0100

    + convenience

--- misaxx/accessors/misa_exported_attachments.h
+++ misaxx/accessors/misa_exported_attachments.h
@@ -15,6 +15,12 @@ namespace misaxx {
      */
     struct misa_exported_attachments : public misa_cached_data<misa_exported_attachments_cache>,
             public misa_description_accessors_from_cache<misa_exported_attachments_cache, misa_exported_attachments> {
+
+        using misa_cached_data<misa_exported_attachments_cache>::suggest_export_location;
+
+        void suggest_export_location(const misa_filesystem &t_filesystem,
+                                     const boost::filesystem::path &t_path);
+
     };
 }
 
