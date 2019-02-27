a9586f9b2d4c2bafd18b687dbb4458c6def5fc17
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-02-27 13:25:55 +0100

    + added additional functions to cache accessors

--- include/misaxx/core/detail/misa_cached_data.h
+++ include/misaxx/core/detail/misa_cached_data.h
@@ -154,4 +154,29 @@ namespace misaxx {
     std::shared_ptr<const misa_location> misa_cached_data<Cache>::get_location_interface() const {
         return data->get_location_interface();
     }
+
+    template<class Cache>
+    void
+    misa_cached_data<Cache>::force_link(const boost::filesystem::path &t_location, misa_data_description t_description) {
+        force_link(t_location, misa_description_storage::with(std::move(t_description)));
+    }
+
+    template<class Cache>
+    void misa_cached_data<Cache>::suggest_link(const boost::filesystem::path &t_location,
+                                               misa_data_description t_description) {
+        suggest_link(t_location, misa_description_storage::with(std::move(t_description)));
+    }
+
+    template<class Cache>
+    void
+    misa_cached_data<Cache>::suggest_create(const filesystem::entry &t_location, misa_data_description t_description) {
+        suggest_create(t_location, misa_description_storage::with(std::move(t_description)));
+    }
+
+    template<class Cache>
+    void misa_cached_data<Cache>::suggest_export_location(const misa_filesystem &t_filesystem,
+                                                          const boost::filesystem::path &t_path,
+                                                          misa_data_description t_description) {
+        suggest_export_location(t_filesystem, t_path, misa_description_storage::with(std::move(t_description)));
+    }
 }
