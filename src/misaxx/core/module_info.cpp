23d5523b75ef1a8870fba7a0363af2106924ca5f
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-12-11 22:48:13 +0100

    * updated module info

--- src/misaxx/core/module_info.cpp
+++ src/misaxx/core/module_info.cpp
@@ -22,7 +22,7 @@ misaxx::misa_module_info misaxx::module_info() {
     info.add_author("Ruman Gerst");
     info.set_license("BSD-2-Clause");
     info.set_organization("Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Institute (HKI), Jena, Germany");
-    info.set_url("https://asb-git.hki-jena.de/RGerst/misaxx-core/");
+    info.set_url("https://applied-systems-biology.github.io/misa-framework/");
 
     // External dependency: Boost
     misaxx::misa_module_info boost_info;
